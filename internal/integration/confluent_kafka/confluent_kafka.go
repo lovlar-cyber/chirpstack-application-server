1c26066c1c2d2842940c40feff7b01388a4771d7
lovlar-cyber <larssonlove@gmail.com>
2021-01-20 22:23:00 +0100

    try to fix undefined: kafka.Producer

--- internal/integration/confluent_kafka/confluent_kafka.go
+++ internal/integration/confluent_kafka/confluent_kafka.go
@@ -23,7 +23,7 @@ import (
 // Integration implements an Confluent Kafka integration.
 type Integration struct {
 	marshaler        marshaler.Type
-	writer           *confluent_kafka.Producer //confluent_kafka instead of kafka
+	writer           *confluent_kafka.Producer 
 	eventKeyTemplate *template.Template
 	config           config.IntegrationConfluentKafkaConfig
 }
